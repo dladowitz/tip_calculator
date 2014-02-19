2f1784d33b011914359ffc21a821da367ed540c4
David Ladowitz <david@ladowitz.com>
2014-02-18 18:56:35 -0800

    Initial Commit

---
+++ tippingCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tippingCalculator
+//
+//  Created by David Ladowitz on 2/18/14.
+//  Copyright (c) 2014 David Ladowitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tippingCalculatorTests/tippingCalculatorTests.m
@@ -0,0 +1,34 @@
+//
+//  tippingCalculatorTests.m
+//  tippingCalculatorTests
+//
+//  Created by David Ladowitz on 2/18/14.
+//  Copyright (c) 2014 David Ladowitz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tippingCalculatorTests : XCTestCase
+
+@end
+
+@implementation tippingCalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
